7487b276cb8b240934aef4a709550dd1f7d8cdc1
Prish20 <prish@students.uonbi.ac.ke>
2024-01-05 06:04:07 +0000

    Create pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+
 export default function App() {
-  return <h1 className="text-blue-500">App</h1>;
+  return (
+    <BrowserRouter>
+     <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/Signup" element={<SignUp />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Profile" element={<Profile />} />
+     </Routes>
+    </BrowserRouter>
+  );
 }
