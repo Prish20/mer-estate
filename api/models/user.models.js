68ce8ec78522f6dbbcf4a9b6aca050e51340decc
Prish20 <prish@students.uonbi.ac.ke>
2024-01-07 17:30:04 +0000

    Update the header and make the profile page private

--- api/models/user.models.js
+++ api/models/user.models.js
@@ -1,26 +1,28 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/01/28/23/24/woman-615421_1280.jpg"
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-    default: "https://www.freepik.com/free-vector/anonymous-avatars-grey-circles_44471922.htm#query=profile&position=0&from_view=keyword&track=sph&uuid=5d338399-8234-4cd6-9a3f-4499cdf319be"
-  },
-
-}, { timestamps: true});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
